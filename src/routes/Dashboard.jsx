2404e88d66561a3af765c88066a0b54a848dd66b
Mitch Hollyfield <130614444+MiMeetch@users.noreply.github.com>
2023-11-12 18:06:23 -0500

    added search feature

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -10,10 +10,16 @@ import IconButton from '@mui/material/IconButton';
 import SettingsIcon from '@mui/icons-material/Settings';
 import CircularProgress from '@mui/material/CircularProgress';
 import { useNavigate } from 'react-router-dom';
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+
+const API_KEY = '08b52ab823f74e3baa0824b66e42a0ac';
 
 export default function Dashboard() {
   const [userName, setUserName] = useState('');
   const [isLoading, setIsLoading] = useState(true);
+  const [searchInput, setSearchInput] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -44,6 +50,48 @@ export default function Dashboard() {
     }
   };
 
+  const handleSearchChange = (event) => {
+    setSearchInput(event.target.value);
+  };
+
+  const fetchNutritionFacts = async (id) => {
+    try {
+      const response = await fetch(
+        `https://api.spoonacular.com/food/ingredients/${id}/information?amount=1&apiKey=${API_KEY}`
+      );
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error('Error fetching nutrition facts: ', error);
+      return null;
+    }
+  };
+
+  const searchIngredients = async () => {
+    try {
+      const response = await fetch(
+        `https://api.spoonacular.com/food/ingredients/search?query=${searchInput}&apiKey=${API_KEY}`
+      );
+      const data = await response.json();
+
+      const resultsWithNutrition = await Promise.all(
+        data.results.map(async (item) => {
+          const nutritionData = await fetchNutritionFacts(item.id);
+          return { ...item, nutrition: nutritionData };
+        })
+      );
+
+      setSearchResults(resultsWithNutrition);
+    } catch (error) {
+      console.error('Error fetching data: ', error);
+    }
+  };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault();
+    searchIngredients();
+  };
+
   return (
     <div>
       <Box sx={{ flexGrow: 1 }}>
@@ -53,7 +101,7 @@ export default function Dashboard() {
               size="large"
               edge="start"
               color="inherit"
-              aria-label="menu"
+              aria-label="settings"
               sx={{ mr: 2 }}
             >
               <SettingsIcon />
@@ -66,7 +114,22 @@ export default function Dashboard() {
                 flexGrow: 1,
               }}
             >
-              {/* put sliders here */}
+              <Box
+                sx={{
+                  minWidth: 500,
+                  maxWidth: '100%',
+                }}
+              >
+                <form onSubmit={handleSearchSubmit}>
+                  <TextField
+                    fullWidth
+                    label="Search Ingredients"
+                    id="search"
+                    value={searchInput}
+                    onChange={handleSearchChange}
+                  />
+                </form>
+              </Box>
             </Box>
             <Button color="inherit" onClick={handleLogout}>
               Logout
@@ -75,11 +138,52 @@ export default function Dashboard() {
         </AppBar>
       </Box>
       {isLoading ? (
-        <Box sx={{ display: 'flex', justifyContent: 'center', m: 'auto' }}>
-          <CircularProgress /> {/* Loading indicator */}
+        <Box sx={{ display: 'flex', justifyContent: 'center', mt: 5 }}>
+          <CircularProgress />
         </Box>
       ) : (
-        <h1>Hello, {userName}</h1>
+        <h1 style={{ textAlign: 'center', marginTop: '20px' }}>
+          Hello, {userName}
+        </h1>
+      )}
+      {searchResults.length > 0 && (
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            gap: '10px',
+            marginTop: '20px',
+          }}
+        >
+          {searchResults.map((item) => (
+            <Paper key={item.id} sx={{ padding: '10px', width: '50%' }}>
+              <h3>{item.name}</h3>
+              {item.nutrition.nutrition &&
+              item.nutrition.nutrition.caloricBreakdown ? (
+                <div>
+                  <p>
+                    % Carbs:{' '}
+                    {item.nutrition.nutrition.caloricBreakdown.percentCarbs ??
+                      'N/A'}
+                  </p>
+                  <p>
+                    % Fat:{' '}
+                    {item.nutrition.nutrition.caloricBreakdown.percentFat ??
+                      'N/A'}
+                  </p>
+                  <p>
+                    % Protein:{' '}
+                    {item.nutrition.nutrition.caloricBreakdown.percentProtein ??
+                      'N/A'}
+                  </p>
+                </div>
+              ) : (
+                <p>Nutrition data not available</p>
+              )}
+            </Paper>
+          ))}
+        </div>
       )}
     </div>
   );
