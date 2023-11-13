f1db38e1d66c97c588913d740937e2805d7325f6
Mitch Hollyfield <130614444+MiMeetch@users.noreply.github.com>
2023-11-12 19:58:32 -0500

    quick fix

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './hooks/useAuth'; // Import the provider
+import { AuthProvider } from './hooks/useAuth';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
